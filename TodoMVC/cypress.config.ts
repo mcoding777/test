37cadfd59b212fe1fd3c5e20a03d5035e22d590e
misun <mcoding777@naver.com>
2022-07-04 10:02:55 +0900

    feat: 테스트 커버리지 보고서 확인 (테스트 파일은 설정 불가능한가?)

--- TodoMVC/cypress.config.ts
+++ TodoMVC/cypress.config.ts
@@ -8,6 +8,13 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.ts')(on, config)
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000'
   },
+  "env": {
+    "codeCoverage": {
+      "expectBackendCoverageOnly": false,
+      "all": true,
+      "include": "src/**/*.*"
+    }
+  }
 })
